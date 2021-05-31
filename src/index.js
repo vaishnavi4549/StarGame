b07bc0a9042e885357867e27905d344eadc5940d
Vaishnavi Kulkarni <Vaishnavi111Kulkarni@gmail.com>
2021-05-31 23:20:47 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import StarMatch from './StarMatch';
+ReactDOM.render(
+    <StarMatch/>,
+  document.getElementById('root'),
+);
+
+reportWebVitals();
